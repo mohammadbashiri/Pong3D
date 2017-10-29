05f47d03e3ed44f41bc14b65508b40f2e6beb9e5
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2017-10-29 19:17:53 +0100

    added some methods, attributes
    the code, now, only includes one scheduled method. And all the other methods are added to the corresponding class.
    
    the angle of the ball bounce now depends on the position where it hits the bat. however, there is still a need for the speed of the ball to depend on the speed of the bat.

--- MyPong2.py
+++ MyPong2.py
@@ -4,123 +4,55 @@ from pyglet.window import key
 import ratcave as rc
 from geometry import Bat, Ball
 
-
-def sind(angle):
-    return np.sin(angle*np.pi/180)
-
-def cosd(angle):
-    return np.cos(angle*np.pi/180)
-
-# Create Window and Add Keyboard State Handler to it's Event Loop
-window = pyglet.window.Window(resizable=True, fullscreen=False)
+# Create Window and Add Keyboard State Handler to its Event Loop
+window = pyglet.window.Window(resizable=True)
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
-# Insert filename into WavefrontReader.
-obj_filename = rc.resources.obj_primitives # this is the path to the obj_primitives .obj file
-obj_reader   = rc.WavefrontReader(obj_filename) # using the WavefrontReader read the .obj file
-
-# Add the "bat"
-bat = Bat(x=-4, y=0, color=(0.6, 0, 0))
-bat2 = Bat(x=4, y=0, color=(0.6, 0, 0))
+# Add the bats
+bat1_control_keys = (key.A, key.D, key.W, key.S)
+bat2_control_keys = (key.LEFT, key.RIGHT, key.UP, key.DOWN)
+bat1 = Bat(x=-4, y=0, color=(0.6, 0, 0), control_keys=bat1_control_keys)
+bat2 = Bat(x=4, y=0, color=(0.6, 0, 0), control_keys=bat2_control_keys)
 
 # Add the ball
-# initialize the movement of the ball
-x = np.random.random() * 6 - 3
+x = np.random.randint(bat1.x+1, bat2.x+-1)
 y = np.random.randint(-3, 3)
-angle = np.random.randint(150,210)
-ball = Ball(x=x, y=y, color=(0.6, 0, 0), angle=angle)
-
-
-print(ball.angle, sind(ball.angle))
-
-#ball_sound = pyglet.media.load('PPB.wav', streaming=False)
+angle = np.random.randint(150, 210)
+ball1 = Ball(x=x, y=y, angle=angle, color=(1, 1, 1), speed=5, sound_path='PPB.wav')
 
 # Create Scene
-scene = rc.Scene(meshes=[ball.mesh, bat.mesh, bat2.mesh])
-scene.bgColor = 0, 0, 0.2 # set the background of thee scene
-# scene.light.position.xyz = 0, 0, -9
-scene.camera = rc.Camera(position=(0, 0, 2), rotation=(0, 0, 0))
+scene = rc.Scene(meshes=[ball1.mesh, bat1.mesh, bat2.mesh])
+scene.bgColor = 0, 0, 0.2  # set the background of thee scene
+scene.camera = rc.Camera(position=(0, 0, 0), rotation=(0, 0, 0))
 scene.camera.projection.z_far = 10
-
-'''
-there is a need for a function that gives the dimension of the object and its position withing
-the scene in terms of pixels, because then it's much easier to relate it to the window size
-'''
-
-'''
-can I change the width and height of the cube here?
-'''
+# scene.light.position.xyz = 0, 0, -9
 
 
-def checkBounce(ballPos, ballRadius, batPos, batDim):
+def update(dt):
 
-    result = [1, 1, 1, 1] # left, right, up, down
+    for bb in [bat1, bat2]:
+        # result = did_bounce(ball1.xyz, ball1.radius, bb.xyz, 1)
+        result = bb.did_bounce(ball1.xyz, ball1.radius, 4, -4)
+        ball1.update_angle(result, dt)
 
-    # left side (x is -ve)
-    boundaryX = (batPos[0] - batDim/2, batPos[0] + batDim/2) # (left, right)
-    boundaryY = (batPos[1] + batDim/2, batPos[1] - batDim/2) # (upper boundary, lower boundary)
-    if ((ballPos[1] - ballRadius) < boundaryY[0]) and ((ballPos[1] + ballRadius) > boundaryY[1]) and batPos[0] < ballPos[0]:
-        if (ballPos[0] - ballRadius) < boundaryX[1]:
-            result[0] = 0
+        if keys[bb.control_keys[0]]:
+            bb.x -= bb.speed * dt
+        if keys[bb.control_keys[1]]:
+            bb.x += bb.speed * dt
+        if keys[bb.control_keys[2]]:
+            bb.y += bb.speed * dt
+        if keys[bb.control_keys[3]]:
+            bb.y -= bb.speed * dt
 
-    elif (ballPos[0] - ballRadius) < -5:
-        result[0] = 0
+        if keys[key.SPACE] or ball1.xyz[0] < -5 or ball1.xyz[0] > 5:
+            ball1.x = np.random.randint(bat1.x+1, bat2.x+-1)
+            ball1.y = np.random.randint(-3, 3)
+            ball1.angle = np.random.randint(150, 210)
 
-    # right side (x is +ve)
-    if ((ballPos[1] - ballRadius) < boundaryY[0]) and ((ballPos[1] + ballRadius) > boundaryY[1]) and batPos[0] > ballPos[0]:
-        if (ballPos[0] + ballRadius) > boundaryX[0]:
-            result[1] = 0
-
-    elif (ballPos[0] + ballRadius) > 5:
-        result[1] = 0
-
-    # upper side (y is +ve)
-    if (ballPos[1] + ballRadius) > 4:
-        result[2] = 0
-
-    # bottom side (y is -ve)
-    if (ballPos[1] - ballRadius) < -4:
-        result[3] = 0
-
-    return result
-
-def ball_bounce(dt):
-
-    for bb in [bat, bat2]:
-        result = checkBounce(ball.xyz, .1, bb.xyz, 1)
-
-        if result[0] == 0 or result[1] == 0:
-            ball.bounce(True)
-        if result[2] == 0 or result[3] == 0:
-            ball.bounce(False)
-
-pyglet.clock.schedule(ball_bounce)
-
-
-def check_keyboard(dt):
-    if keys[key.A]:
-        bat.x -= bat.speed * dt
-    if keys[key.D]:
-        bat.x += bat.speed * dt
-    if keys[key.W]:
-        bat.y += bat.speed * dt
-    if keys[key.S]:
-        bat.y -= bat.speed * dt
-    if keys[key.LEFT]:
-        bat2.x -= bat2.speed * dt
-    if keys[key.RIGHT]:
-        bat2.x += bat2.speed * dt
-    if keys[key.UP]:
-        bat2.y += bat2.speed * dt
-    if keys[key.DOWN]:
-        bat2.y -= bat2.speed * dt
-    if keys[key.SPACE]:
-        globals()['ball'] = Ball(x, y, color=(0.6, 0, 0), angle=angle)
-
-pyglet.clock.schedule(check_keyboard)
-
+pyglet.clock.schedule(update)
 shader = rc.Shader.from_file(*rc.resources.genShader)
+
 
 @window.event
 def on_draw():
--- geometry.py
+++ geometry.py
@@ -1,38 +1,43 @@
 import ratcave as rc
-import numpy as np
 import pyglet
+import numpy as np
 
-obj_reader = rc.WavefrontReader(rc.resources.obj_primitives)
+def sind(angle):
+    return np.sin(angle*np.pi/180)
+
+def cosd(angle):
+    return np.cos(angle*np.pi/180)
+
+obj_filename = rc.resources.obj_primitives
+obj_reader = rc.WavefrontReader(obj_filename)
 
 class Geometry(object):
 
-    def __init__(self, mesh, x, y, color, speed=10., scale=0.5):
-        pass
+    def __init__(self, mesh, x, y, speed=0, scale=1, color=(1, 1, 1)):
 
         self.mesh = obj_reader.get_mesh(mesh, position=(0, 0, -7), scale=scale)
         self.mesh.uniforms['diffuse'] = color
+        self.speed = speed
         self.x = x
         self.y = y
-        self.speed = speed
-
 
     @property
     def x(self):
-        return self.__x
+        return self._x
 
     @x.setter
-    def x(self, value):
-        self.__x = value
-        self.mesh.position.x = value
+    def x(self, val):
+        self._x = val
+        self.mesh.position.x = val
 
     @property
     def y(self):
-        return self.__y
+        return self._y
 
     @y.setter
-    def y(self, value):
-        self.__y = value
-        self.mesh.position.y = value
+    def y(self, val):
+        self._y = val
+        self.mesh.position.y = val
 
     @property
     def xyz(self):
@@ -40,30 +45,78 @@ class Geometry(object):
 
 
 class Bat(Geometry):
-    def __init__(self, x, y, color):
-        super().__init__(mesh='Cube', x=x, y=y, color=color, scale=0.5)
+
+    def __init__(self, x, y, color=(1, 1, 1), scale=0.5, speed=10, control_keys=None):
+        super().__init__(mesh="Cube", x=x, y=y, color=color, scale=1, speed=speed)
+
+        self.control_keys = control_keys
+
+        # change the size of any dimension of the object
+        self.mesh.arrays[0][:, 0:3] *= scale
+        self.mesh.arrays[0][:, 0] *= 0.05  # x-dimension
+        self.mesh.arrays[0][:, 1] *= 1.5  # y-dimension
+        self.length_x = 2 * np.mean((abs(np.min(self.mesh.arrays[0][:, 0])), np.max(self.mesh.arrays[0][:, 0])))
+        self.length_y = 2 * np.mean((abs(np.min(self.mesh.arrays[0][:, 1])), np.max(self.mesh.arrays[0][:, 1])))
+
+    def did_bounce(self, ball_xyz, ball_radius, upper_wall_y, lower_wall_y):
+
+        # left, right, up, down, left hit angle increment, right hit angle increment
+        did_bounce_result = [False, False, False, False, 0, 0]
+        boundary_y = (self.y - self.length_y/2, self.y + self.length_y/2)
+
+        # check for the collision of the ball and the bat within the y-range of the bat
+        if (ball_xyz[1] > boundary_y[0]) and (ball_xyz[1] < boundary_y[1]):
+
+            # this is both ball and bat are in the left half of the field
+            if (ball_xyz[0] < 0) and (self.x < 0) and ((ball_xyz[0] - ball_radius) < self.x + self.length_x/2) \
+                    and ((ball_xyz[0] + ball_radius) > self.x - self.length_x/2):
+
+                did_bounce_result[0] = True
+                did_bounce_result[4] = (ball_xyz[1] - self.y) / self.length_y * 2 * 20
+
+            # this is both ball and bat are in the left half of the field
+            if (ball_xyz[0] > 0) and (self.x > 0) and ((ball_xyz[0] + ball_radius) > self.x - self.length_x/2) \
+                    and ((ball_xyz[0] - ball_radius) < self.x + self.length_x/2):
+
+                did_bounce_result[1] = True
+                did_bounce_result[5] = -(ball_xyz[1] - self.y) / self.length_y * 2 * 20
+
+        # upper wall (y is +ve)
+        if (ball_xyz[1] + ball_radius) > upper_wall_y:
+            did_bounce_result[2] = True
+
+        # bottom wall (y is -ve)
+        if (ball_xyz[1] - ball_radius) < lower_wall_y:
+            did_bounce_result[3] = True
+
+        return did_bounce_result
 
 
 class Ball(Geometry):
 
-    def __init__(self, x, y, color, speed=1., angle=170):
-        super().__init__(mesh='Sphere', x=x, y=y, color=color, scale=0.2)
-        self.speed = speed
+    def __init__(self, x, y, angle, color=(1, 1, 1), scale=0.1, speed=10, sound_path=None):
+        super().__init__(mesh="Sphere", x=x, y=y, color=color, scale=1, speed=speed)
+
         self.angle = angle
-        # self.sound = pyglet.media.load('PPB.wav', streaming=False)
-        pyglet.clock.schedule(self.update)
+        self.speed = speed
+        self.sound = pyglet.media.load(sound_path, streaming=False)
 
-    def update(self, dt):
-        self.x += self.speed * np.cos(np.degrees(self.angle)) * dt
-        self.y += self.speed * np.sin(np.degrees(self.angle)) * dt
+        # change the size in any dimension
+        self.mesh.arrays[0][:, 0:3] *= scale
 
-    def bounce(self, bounce_dir):
-        if bounce_dir:
-            self.angle = 180 - self.angle
-            print('Bounced left/right!!')
-        else:
-            self.angle = 180 - self.angle
-            print('Bounced up/down!!')
+        # calculate the radius of the ball
+        self.radius = np.mean((abs(np.min(self.mesh.arrays[0][:, 0])), np.max(self.mesh.arrays[0][:, 0])))
 
-        # self.sound.play()
+    def update_angle(self, result, dt):
 
+        if result[0] or result[1]:
+            self.angle = 180 - self.angle + result[4] + result[5]
+            self.sound.play()
+
+        if result[2] or result[3]:
+            self.angle = 360 - self.angle
+            self.sound.play()
+
+        # print(ball_angle)
+        self.x += self.speed * cosd(self.angle) * dt
+        self.y += self.speed * sind(self.angle) * dt
