d3ec73016d7e5fc1b6904bac065e8f36216aea1e
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2017-11-13 12:24:00 +0100

    reeverted the changes

--- MyPong2.py
+++ MyPong2.py
@@ -3,11 +3,17 @@ import numpy as np
 from pyglet.window import key
 import ratcave as rc
 from geometry import Bat, Ball
+from os.path import dirname, split, isdir
 
 # Create Window and Add Keyboard State Handler to its Event Loop
 window = pyglet.window.Window(resizable=True)
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
+
+# add the arena
+path_arena = r"D:\TUM\Work\Sirota\My Models\arena\arenaj2.obj"
+print(split(path_arena))
+
 
 # Add the bats
 bat1_control_keys = (key.A, key.D, key.W, key.S)
@@ -28,29 +34,29 @@ scene.camera = rc.Camera(position=(0, 0, 0), rotation=(0, 0, 0))
 scene.camera.projection.z_far = 10
 # scene.light.position.xyz = 0, 0, -9
 
-
-def update(dt):
-
-    for bb in [bat1, bat2]:
-        # result = did_bounce(ball1.xyz, ball1.radius, bb.xyz, 1)
-        result = bb.did_bounce(ball1.xyz, ball1.radius, 4, -4)
-        ball1.update_angle(result, dt)
-
-        if keys[bb.control_keys[0]]:
-            bb.x -= bb.speed * dt
-        if keys[bb.control_keys[1]]:
-            bb.x += bb.speed * dt
-        if keys[bb.control_keys[2]]:
-            bb.y += bb.speed * dt
-        if keys[bb.control_keys[3]]:
-            bb.y -= bb.speed * dt
-
-        if keys[key.SPACE] or ball1.xyz[0] < -5 or ball1.xyz[0] > 5:
-            ball1.x = np.random.randint(bat1.x+1, bat2.x+-1)
-            ball1.y = np.random.randint(-3, 3)
-            ball1.angle = np.random.randint(150, 210)
-
-pyglet.clock.schedule(update)
+#
+# def update(dt):
+#
+#     for bb in [bat1, bat2]:
+#         # result = did_bounce(ball1.xyz, ball1.radius, bb.xyz, 1)
+#         result = bb.did_bounce(ball1.xyz, ball1.radius, 4, -4)
+#         ball1.update_angle(result, dt)
+#
+#         if keys[bb.control_keys[0]]:
+#             bb.x -= bb.speed * dt
+#         if keys[bb.control_keys[1]]:
+#             bb.x += bb.speed * dt
+#         if keys[bb.control_keys[2]]:
+#             bb.y += bb.speed * dt
+#         if keys[bb.control_keys[3]]:
+#             bb.y -= bb.speed * dt
+#
+#         if keys[key.SPACE] or ball1.xyz[0] < -5 or ball1.xyz[0] > 5:
+#             ball1.x = np.random.randint(bat1.x+1, bat2.x+-1)
+#             ball1.y = np.random.randint(-3, 3)
+#             ball1.angle = np.random.randint(150, 210)
+#
+# pyglet.clock.schedule(update)
 shader = rc.Shader.from_file(*rc.resources.genShader)
 
 
